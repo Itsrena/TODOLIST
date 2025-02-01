901d93078e0d9e42a2f98e93c23de207959e9cb2
Itsrena <reginazumagulova832@gmail.com>
2025-02-01 08:21:27 +0300

    Add files via upload

---
+++ 6546R5TR/script.js
@@ -0,0 +1,42 @@
+let main = document.createElement('main')
+main.className='container'
+
+document.body.prepend(main)
+
+let projectName = document.createElement('h1')
+projectName.innerHTML="let's do it"
+main.append(projectName)
+
+let listBlock = document.createElement('div')
+listBlock.className='mainBlock'
+main.append(listBlock)
+
+let firsDiv = document.createElement('div')
+listBlock.append(firsDiv)
+
+let texIn = document.createElement('input')
+texIn.className= 'texIn'
+texIn.setAttribute('placeeholder','Gonna do...')
+firsDiv.append(texIn)
+
+let setDate = document.createElement('input')
+setDate.setAttribute('type','date')
+firsDiv.append(setDate)
+
+let addBtn = document.createElement('button')
+addBtn.innerHTML = 'ADD'
+addBtn.id = 'AddBtn'
+firsDiv.append(addBtn)
+
+let list = document.createElement('ul')
+listBlock.append(list)
+
+const addTodo = () => {
+    let li = document.createElement('li')
+    li.className = 'taskItem'
+
+
+    list.append(li)
+}
+
+addBtn.addEventListener('click', addTodo)
